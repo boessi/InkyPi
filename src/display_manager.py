d524cb80c4e008061b10150e72b6ec31363da586
boessi <dario.bosshard@flicks.ch>
2025-05-27 15:21:50 +0200

    Fix

--- src/display_manager.py
+++ src/display_manager.py
@@ -11,52 +11,11 @@ import importlib
 class DisplayManufactureType(Enum):
     InkyImpression = 1
     Waveshare = 2
-class DisplayManufactureType(Enum):
-    InkyImpression = 1
-    Waveshare = 2
 
 class DisplayManager:
     def __init__(self, device_config):
         """Manages the display and rendering of images."""
         self.device_config = device_config
-
-        self.display_type = None
-        self.display_module = None
-        self.display_instance = None
-        self.display_manufacture_type = None
-
-    def init_module(self):
-        device_type = self.device_config.get_config("deviceType")
-
-        if self.display_type is None or self.display_type != device_type:
-            self.display_type = device_type
-            self.display_module = None
-            self.display_instance = None
-
-        if self.display_module is None:
-            # Dynamically import the module
-            if not (bool(self.display_type and self.display_type.strip()) and importlib.util.find_spec(self.display_type) is not None):
-                return False
-
-            self.display_module = importlib.import_module(self.display_type)
-
-        if self.display_module is None:
-            return False
-
-        self.display_manufacture_type = DisplayManufactureType.Waveshare if hasattr(self.display_module, "EPD") else DisplayManufactureType.InkyImpression
-
-        if self.display_manufacture_type == DisplayManufactureType.Waveshare:
-            if self.display_instance is None:
-                self.display_instance = self.display_module.EPD()
-
-            if hasattr(self.display_instance, "init"):
-                self.display_instance.init()
-            else:
-                self.display_instance.Init()
-
-        elif self.display_instance is None:
-            self.display_instance = self.display_module.auto()
-            self.display_instance.set_border(self.display_instance.BLACK)
 
         self.display_type = None
         self.display_module = None
